2364b47131d0c7e5b1f7333c468447241db99034
arjunbista <bistaarjun0@gmail.com>
2019-11-11 13:27:48 +0545

    Input Calculator

---
+++ venv/User.py
@@ -0,0 +1,14 @@
+input("Name:")
+input("age:")
+first=int(input("enter first no="))
+Second=int(input("enter second no="))
+add=first+Second
+sub=first-Second
+mul=first*Second
+div=first/Second
+print("The add result=",add)
+print("The sub result=",sub)
+print("The mul result=",mul)
+print("The div result=%.2f" %div)
+
+
