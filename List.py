81ba2cf54b4ba1dd68789a8055402b9da7aaf51d
arjunbista <bistaarjun0@gmail.com>
2019-11-12 15:47:30 +0545

    List functions and replace functions

---
+++ List.py
@@ -0,0 +1,37 @@
+
+lst = []  #SUM Of elements from list#
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Sum of elements in given list is :", sum(lst))
+
+b=input("Enter your Name: ")   #String count from#
+a=input("Which string to count: ")
+
+print(b.count(a))
+
+list=[1,4,2,32,9,45]
+print(max(list))   #print max#
+print(min(list))
+
+
+str="I believe your aim and i Stay on it"
+print(str.upper())   # Conversion to UpperCase#
+print(str.lower())   # Conversion To LOWER CASE. #
+
+
+print((len(str)))   # LENGTH of string.#
+print(str.replace("i","We"))  #Word to string conversion#
+print(str.replace("e","E",3))   #with count, 3 times replacing.#
+
+
+
+
+
+
+
+
+
+
+
